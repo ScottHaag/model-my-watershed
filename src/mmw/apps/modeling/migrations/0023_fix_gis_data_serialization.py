11c1c594be3033ba67f7b5db4601b93b7bb2c50b
Justin Walgran <jwalgran@azavea.com>
2017-09-29 09:21:01 -0700

    FIXUP - Exclude empty and already JSON from serialization fix

--- src/mmw/apps/modeling/migrations/0023_fix_gis_data_serialization.py
+++ src/mmw/apps/modeling/migrations/0023_fix_gis_data_serialization.py
@@ -9,10 +9,8 @@ from django.db import migrations
 
 def fix_gis_data_serialization(apps, schema_editor):
     Project = apps.get_model('modeling', 'Project')
-    for project in Project.objects.all():
-        if project.gis_data:
-            # literal_eval works for both JSON and str(dict)
-            # https://docs.python.org/2.7/library/ast.html#ast.literal_eval
+    for project in Project.objects.exclude(gis_data=''):
+        if "u'" in project.gis_data:
             project.gis_data = json.dumps(ast.literal_eval(project.gis_data))
             project.save()
 
